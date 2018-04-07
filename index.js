29a8a35226d5af0efd7e6b1b476c9e514e02e8b7
Lorenzo Salamante <lsalamante@myoptimind.com>
2018-04-07 12:03:34 +0800

    Add the initial retweet code

---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+
+const Twit = require('twit')
+const T = new Twit({
+    consumer_key:         process.env.CONSUMER_KEY,
+    consumer_secret:      process.env.CONSUMER_SECRET,
+    access_token:         process.env.ACCESS_TOKEN,
+    access_token_secret:  process.env.ACCESS_TOKEN_SECRET,
+})
+
+var users = ["122569945"];
+var stream = T.stream('statuses/filter', {follow: users});
+stream.on('tweet', function (tweet) {
+    if (users.indexOf(tweet.user.id_str) > -1) {
+        console.log(tweet.user.name + ": " + tweet.text);
+        T.post('statuses/retweet/:id', { id: tweet.id_str }, function (err, data, response) {
+            console.log(data)
+        })
+    }
+})
